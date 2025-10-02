4572378b06beebe880b8db0cb5d65c6e84eed13e
Jason Warren <jason@foundersandcoders.com>
2025-10-02 17:19:20 +0100

    build: initialise svelte-kit with correct nvm #

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="prose">
+  <p>Created by <a href="https://github.com/neurosocialist">Jason Warren</a> & Harriet Layhe</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import Timeline from '$lib/components/Timeline.svelte';
+  import Tasks from '$lib/components/Tasks.svelte';
+  
+  onMount(() => {
+    console.log('Mounted');
+  });
+</script>
+
+<main>
+  <Tasks />
+
+  <Timeline />
+</main>
