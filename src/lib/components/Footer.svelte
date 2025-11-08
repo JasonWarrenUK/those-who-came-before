026685aba304ebdaa791b5af4426f888a7dcc747
Jason Warren <jason@foundersandcoders.com>
2025-11-08 17:33:17 +0000

    fix: correct link to github.com/JasonWarrenUK

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
 <footer class="prose">
-  <p>Created by <a href="https://github.com/neurosocialist">Jason Warren</a> & Harriet Layhe</p>
+  <p>Created by <a href="https://github.com/JasonWarrenUK">Jason Warren</a> & Harriet Layhe</p>
 </footer>
